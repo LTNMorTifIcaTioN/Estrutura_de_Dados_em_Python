9aedd80993a35883bfaf6548d7a463cee1a76cfe
Nayan Couto <pereira.nayan@gmail.com>
2023-05-24 22:35:29 -0300

    UnidadeI.AulaII

--- projetos_pycharm/unidade1_aula2/ListaEncadeada.py
+++ projetos_pycharm/unidade1_aula2/ListaEncadeada.py
@@ -5,7 +5,7 @@ class ItemLista:
         self.data = data
         self.nextItem = nextItem
     def __repr__(self):
-        return '$s => %s' % (self.data, self.nextItem)
+        return '%s => %s' % (self.data, self.nextItem)
 
 class ListaEncadeada:
     """Cria uma lista encadeada"""
@@ -20,3 +20,29 @@ class ListaEncadeada:
         item.nextItem = lista.head
         # o item atual se torna o head
         lista.head = item
+
+    # aqui a função busca itens na lista
+    def busca(lista,valor):
+        navegar = lista.head
+        while navegar and navegar.data != valor:
+            navegar = navegar.nextItem
+        return navegar
+
+    # método de remoção de itens
+    def remove(self, valor):
+        # verificamos se o item se trata do head
+        if self.head.data == valor:
+            self.head = self.head.nextItem
+        else:
+            # detectando a posição do elemento
+            before = None
+            navegar = self.head
+            # navega pelo cabeçalho atual
+            while navegar and navegar.data != valor:
+                before = navegar
+                navegar = navegar.nextItem
+            # remove o item atual
+            if navegar:
+                before.nextItem = navegar.nextItem
+            else:
+                before.nextItem = None
--- projetos_pycharm/unidade1_aula2/processa.py
+++ projetos_pycharm/unidade1_aula2/processa.py
@@ -2,3 +2,33 @@ import ListaEncadeada as le
 # Cria o objeto
 lista = le.ListaEncadeada()
 print("Conteúdo da lista: ", lista)
+print()
+# lista está vazia
+
+# Cria a lista
+le.ListaEncadeada.insere(lista, "abacate")
+le.ListaEncadeada.insere(lista, "biscoito")
+le.ListaEncadeada.insere(lista, "cenoura")
+le.ListaEncadeada.insere(lista, "desodorante")
+le.ListaEncadeada.insere(lista, "espinafre")
+print(lista)
+print()
+# espinafre => desodorante => cenoura => biscoito => abacate => None
+
+# Realiza uma busca:
+query = "cenoura"
+item_buscado = le.ListaEncadeada.busca(lista, query)
+if item_buscado:
+    print("Elemento encontrado")
+else:
+    print("Elemento não encontrado")
+print()
+# Neste caso, a busca irá retornar "Elemento Encontrado"
+
+# Realiza a deleção:
+print(lista)
+print()
+# espinafre => desodorante => cenoura => biscoito => abacate => None
+le.ListaEncadeada.remove(lista,"cenoura")
+print(lista)
+# espinafre => desodorante => biscoito => abacate => None
